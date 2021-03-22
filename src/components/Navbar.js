21881e306a1a995a57e21ef7abe88f078f5f430e
Tipe123 <monyemangene.2@gmail.com>
2021-03-23 00:25:13 +0200

    Playing with a non functional Authentication

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React , { Component } from 'react'
+import { AuthContext } from '../context/AuthContext';
 import { ThemeContext } from '../context/ThemeContext';
 
 class Navbar extends Component{
     static contextType = ThemeContext;
+
     render(){
         const { isLightTheme, light, dark } = this.context;
-
         const theme = isLightTheme ? light : dark;
         return(
-            <nav style={{background: theme.ui,color: theme.syntax}}>
+            <AuthContext.Consumer>{ (authContext) =>{ //Another way to use Context
+                const {isAuthenticated , toggleAuth}= authContext;
+                return (
+                <nav style={{background: theme.ui,color: theme.syntax}}>
                 <h1>Context App</h1>
+                <div onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged in" :"Logged Out"}
+                </div>
                 <ul>
                     <li>Home</li>
                     <li>About</li>
@@ -17,6 +24,9 @@ class Navbar extends Component{
                     <li>Contact</li>
                 </ul>
             </nav>
+                )
+    }}</AuthContext.Consumer>
+           
         );
     }
 }
